4b0b95e9b08cc6088df8815414029353031d8bcd
aizwellenstan <aizwellenstan@gmail.com>
2020-06-26 22:02:47 +0900

    hiyoki with facedectect

---
+++ debug_tool/test-socket-client.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import socket
+
+
+print('The client started at', datetime.now())
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # s.connect(('127.0.0.1', 50000))
+    s.connect(('127.0.0.1', 5066))
+    s.sendall(b'Hello TCP')
+    data = s.recv(1024)
+    print(repr(data))
---
+++ debug_tool/test-socket-server.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import socket
+
+
+print('The server started at', datetime.now())
+print('Waiting...')
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # s.bind(('127.0.0.1', 50000))
+    s.bind(('127.0.0.1', 5066))
+    s.listen(1)
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print('data: ', data, 'addr: ', addr)
+                conn.sendall(b'Receive: ' + data)
